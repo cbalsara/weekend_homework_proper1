82b2e62a53e2b2c3e63b1cbe2a26aa41d821156a
Cyrus <cdbalsara@icloud.com>
2016-09-30 17:34:59 +0100

    Question 7 added

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,6 +1,6 @@
 require ( "pry-byebug" )
 #binding.pry
-#ruby specs/pet_shop_spec.rb
+#ruby specs/pet_shop_spec.rb for testing in the terminal
 
 def pet_shop_name(property) 
   return property[:name]
@@ -10,7 +10,7 @@ def total_cash(money)
   return money[:admin][:total_cash]
 end  
 
-def add_or_remove_cash(money_being_placed, blank_value) 
+def add_or_remove_cash(money_being_placed, blank_value) #these are here because of the two variables given in the spec question i.e @pet_shop and the number 2
   return money_being_placed[:admin][:total_cash] += blank_value    
 end 
 
@@ -19,7 +19,7 @@ def add_or_remove_cash(money_being_placed, blank_value)
 end 
 
 def pets_sold(animals_at_shop)
-  return animals_at_shop[:admin][:pets_sold]
+  return animals_at_shop[:admin][:pets_sold] 
 end  
 
 def increase_pets_sold(shop_property, increased_number)
@@ -27,6 +27,10 @@ def increase_pets_sold(shop_property, increased_number)
 end 
 
 
+
+def stock_count(shop_property)
+  return shop_property[:pets].length
+end   
 
 
 
