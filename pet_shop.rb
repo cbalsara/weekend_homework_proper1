830a65395656cc968b8871ff70c5fa4c0406658a
Cyrus <cdbalsara@icloud.com>
2016-09-30 16:25:57 +0100

    quesiton 2 added

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,10 +1,15 @@
 require ( "pry-byebug" )
-
+#binding.pry
 
 def pet_shop_name(property) 
   return property[:name]
- 
 end 
+
+def total_cash(money)
+  return money[:admin][:total_cash]
+end  
+
+
 
 
 
