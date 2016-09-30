28061600033878b5fcf5f18ff05953b56c5e4d4d
Cyrus <cdbalsara@icloud.com>
2016-09-30 17:11:35 +0100

    questions 3, 4, 5 added- the rush of the green distracted me

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,5 +1,6 @@
 require ( "pry-byebug" )
 #binding.pry
+#ruby specs/pet_shop_spec.rb
 
 def pet_shop_name(property) 
   return property[:name]
@@ -7,6 +8,18 @@ end
 
 def total_cash(money)
   return money[:admin][:total_cash]
+end  
+
+def add_or_remove_cash(money_being_placed, blank_value) 
+  return money_being_placed[:admin][:total_cash] += blank_value    
+end 
+
+def add_or_remove_cash(money_being_placed, blank_value) 
+  return money_being_placed[:admin][:total_cash] += blank_value    
+end 
+
+def pets_sold(animals_at_shop)
+  return animals_at_shop[:admin][:pets_sold]
 end  
 
 
